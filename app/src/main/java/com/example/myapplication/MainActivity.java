d000b3cd03eea7f316a0da1b8b0c5a39de2ef9f0
Ella-Jr <elhanan027@gmail.com>
2023-03-28 11:10:10 +0300

    BMI calculetor

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextInputEditText height =findViewById(R.id.Height);
+        TextInputEditText weight =findViewById(R.id.Weight);
+        TextView text =findViewById(R.id.textview);
+        Button cal=findViewById(R.id.Calculate);
+        height.setEnabled(true);
+        weight.setEnabled(true);
+        Button res=findViewById(R.id.reset);
+
+        cal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                double h = Double.parseDouble(height.getText().toString());
+                double w = Double.parseDouble(weight.getText().toString());
+                double bmi = w / (h * h);
+                text.setText(Double.toString(bmi) );
+                text.setText("");
+            }
+
+    });
+        res.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            height.setText("");
+            weight.setText("");
+            text.setText("");
+        }
+    });
+    }
+}
